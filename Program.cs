b2b10780ec3bc8e905b8d7e7acee59570d6ce642
gilrsantana <g.r.santana@hotmail.com>
2022-10-29 09:17:39 -0300

    Definições no Razor e Rotas

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
 var app = builder.Build();
+app.UseDeveloperExceptionPage();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+	// Permite criar uma rota personalizada
+	// endpoints.MapControllerRoute("Default", 
+	// "{controller=Home}/{action=Index}/{}id?");
+
+	// Define a rota padrão como a utilizada 
+	// "{controller=Home}/{action=Index}/{}id?"
+	endpoints.MapDefaultControllerRoute();
+});
 
 app.MapGet("/", () => "Hello World!");
 
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # PROJETO USANDO VIEWS
 Projeto web utilizando a tecnologia ASPNET a partir de um projeto web vazio.
+
+## Definições do Projeto
+- Adicionar o Runtime Compilation do Razor.
+
+```
+dotnet add package Microsoft.AspNETCore.Mvc.Razor.RunTimeCompilation
+```
+- Adicionar ao projeto a capacidade de trabalhar com views e utilizar o runtime rompilation do razor.
+- Adicione o código abaixo no arquivo Program.cs
+
+```
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+```
